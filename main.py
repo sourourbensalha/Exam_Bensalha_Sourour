b60703ab8ffc72851337b9631f8a980dd7b3904b
Sourour Ben Salha <sbensalha69@gmail.com>
2025-03-14 10:06:47 +0100

    committttt

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ som=somme(my_list)
 print('la somme est=',som)
 
 my_list=[1,5,7]
-print('la somme est:' sum(my_list))
-print('le max est:' max(my_list))
-print('le min est:' min(my_list))
-
+if my_list:
+   print('la somme est:' sum(my_list))
+   print('le max est:' max(my_list))
+   print('le min est:' min(my_list))
+else:
+   print('liste vide')
