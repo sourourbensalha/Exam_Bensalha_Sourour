b00166fec884e84a2c055102e82b8f57bec64817
Sourour Ben Salha <sbensalha69@gmail.com>
2025-03-14 10:05:23 +0100

    commit 2

--- main.py
+++ main.py
@@ -9,4 +9,7 @@ som=somme(my_list)
 print('la somme est=',som)
 
 my_list=[1,5,7]
-print('la somme est',sum(my_list))
+print('la somme est:' sum(my_list))
+print('le max est:' max(my_list))
+print('le min est:' min(my_list))
+
